cb2d62c0137e2b0999aec56dacd7176d126bfd1c
LagaTech <alamin12071985@gmail.com>
2026-05-09 23:21:42 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+from telethon import TelegramClient, events
+import asyncio
+import time
+import os
+
+===== ENV INFO =====
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+
+client = TelegramClient("laga_session", api_id, api_hash)
+
+OFFLINE_TIME = 300
+
+last_activity = time.time()
+replied_users = set()
+
+AUTO_REPLY = """
+⏩আমি এখন অফলাইনে আছি। অনলাইনে আসলে আপনাকে রিপ্লাই দিব। কারেন্ট থাকে না তাই একটু দেরি হতে পারে ✅🥺
+
+✅Socia Media যে কোনো Service নিতে এই বট ব্যবহার করুন💙💙
+
+🔵 @lagasmmbot
+
+⏩এখানে সবকিছু কম মূল্যে পেয়ে যাবেন।⏩
+
+✉️Join My Channel 🪙✅💎
+@lagatech
+"""
+
+@client.on(events.NewMessage(outgoing=True))
+async def update_activity(event):
+global last_activity
+last_activity = time.time()
+
+@client.on(events.NewMessage(incoming=True))
+async def auto_reply(event):
+global last_activity
+
+if event.sender.bot:
+    return
+
+if event.is_group or event.is_channel:
+    return
+
+user_id = event.sender_id
+
+if time.time() - last_activity > OFFLINE_TIME:
+
+    if user_id not in replied_users:
+        try:
+            await event.reply(AUTO_REPLY)
+            replied_users.add(user_id)
+        except:
+            pass
+
+async def clear_replied():
+global replied_users
+
+while True:
+    if time.time() - last_activity < OFFLINE_TIME:
+        replied_users.clear()
+
+    await asyncio.sleep(60)
+
+async def main():
+asyncio.create_task(clear_replied())
+print("Auto Reply Running...")
+await client.start()
+await client.run_until_disconnected()
+
+with client:
+client.loop.run_until_complete(main())
